ccf7ddd291bc59b72a2649f957c91bd035927b10
hritix <hritikix@gmail.com>
2025-10-25 14:04:42 +0530

    Fixed some isues for the terminals using kitty image protocol, by using `kitten icat`  instead of `chafa`

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 Play music from the terminal using the [ytmusicapi](https://github.com/sigma67/ytmusicapi)
 
-*Dependencies*: [mpv](mpv.io),[yt-dlp](https://github.com/yt-dlp/yt-dlp),[fzf](https://github.com/junegunn/fzf)
+*Dependencies*: [mpv](mpv.io),[yt-dlp](https://github.com/yt-dlp/yt-dlp),[fzf](https://github.com/junegunn/fzf),[curl](https://github.com/curl/curl),[chafa](https://github.com/hpjansson/chafa/)
+
--- fzf-preview.sh
+++ fzf-preview.sh
@@ -5,7 +5,16 @@ ENABLE_IMAGE_PREVIEW=true
 imgprv() {
     img_file="$1"
 	final_dims="${FZF_PREVIEW_COLUMNS}x${FZF_PREVIEW_LINES}"
-	chafa -s "$final_dims" -- "$img_file"	
+	if [[ $TERM = xterm-kitty || $TERM = xterm-ghostty ]];then
+		err_file=`mktemp`
+		kitten icat --clear --transfer-mode=stream --unicode-placeholder --stdin=no --place="${final_dims}@0x0" -- "$img_file" 2>$err_file
+		[[ -s $err_file ]] && curl -sL "$img_url" > $img_file && imgprv "$img_file" "$dim"
+	else
+		chafa -s "$final_dims" -- "$img_file" 2>/dev/null
+		if [ $? -ne 0 ];then
+			curl -sL "$img_url" > $img_file && imgprv "$img_file" "$dim"
+		fi
+	fi
 }
 
 url=`printf "$1" | cut -d ":" -f5`
